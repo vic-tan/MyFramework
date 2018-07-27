addfecd35a1cc221e5eefe3c9a1fbd45948e7c6f
tanlifei <179840045@qq.com>
2018-07-27 16:43:08 +0800

    update

--- app/src/main/java/com/vic/framework/common/base/ui/activity/base/BaseActivity.java
+++ app/src/main/java/com/vic/framework/common/base/ui/activity/base/BaseActivity.java
@@ -1,16 +1,21 @@
 package com.vic.framework.common.base.ui.activity.base;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.KeyboardUtils;
+import com.vic.framework.common.utils.StatusBarManager;
+import com.kaopiz.kprogresshud.KProgressHUD;
 import com.vic.framework.R;
 import com.vic.framework.common.utils.CommTool;
-import com.vic.framework.common.utils.StatusBarManager;
 import com.zhy.autolayout.AutoLayoutActivity;
 
 import butterknife.ButterKnife;
@@ -20,15 +25,34 @@ import butterknife.ButterKnife;
  */
 
 public abstract class BaseActivity extends AutoLayoutActivity {
-    private FrameLayout baseContent;
-    private Toolbar toolbar;
-    private TextView toolbarTitleTv;
-
+    protected FrameLayout baseContent;
+    protected Toolbar toolbar;
+    protected TextView toolbarTitleTv;
+    protected KProgressHUD loading_dialog;
+    protected Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.base_activity);
+        initLoadingDialog();
         initControlViews();
+    }
+
+    protected void initLoadingDialog(){
+        loading_dialog = KProgressHUD.create(this)
+                .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                .setLabel("  加载中...  ")
+                .setCancellable(true)
+                .setAnimationSpeed(2)
+                .setDimAmount(0.5f);
+    }
+
+    public KProgressHUD getLoading_dialog() {
+        if(null == loading_dialog){
+            initLoadingDialog();
+        }
+        return loading_dialog;
     }
 
     /**
@@ -40,7 +64,6 @@ public abstract class BaseActivity extends AutoLayoutActivity {
         toolbarTitleTv =  findViewById(R.id.toolbar_title_tv);
 
         //设置相关默认操作
-        setTitleNavigationIcon(R.mipmap.com_base_back);
         setTitleBgColor(R.color.colorPrimary);
         new StatusBarManager.builder(this)
                 .setStatusBarColor(R.color.colorPrimary)//状态栏颜色
@@ -48,9 +71,9 @@ public abstract class BaseActivity extends AutoLayoutActivity {
                 .setAlpha(0)//不透明度
                 .create();
         setInflateMenu();
-
         baseContent.addView(LinearLayout.inflate(this, setContentViewId(), null));
         ButterKnife.bind(this,baseContent);
+        setHideNavigationIcon(false);
         initViews();
         //左边Navigation Button监听回调
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -67,6 +90,152 @@ public abstract class BaseActivity extends AutoLayoutActivity {
             }
         });
     }
+
+    /** 是否要隐藏返回键 */
+    public void setHideNavigationIcon(boolean hideNavigationIcon){
+        if(!hideNavigationIcon){
+            setTitleNavigationIcon(R.mipmap.com_base_back);
+        }
+    }
+
+    //region软键盘的处理
+
+    /**
+     * 清除editText的焦点
+     *
+     * @param v   焦点所在View
+     * @param ids 输入框
+     */
+    public void clearViewFocus(View v, int... ids) {
+        if (null != v && null != ids && ids.length > 0) {
+            for (int id : ids) {
+                if (v.getId() == id) {
+                    v.clearFocus();
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+    /**
+     * 隐藏键盘
+     *
+     * @param v   焦点所在View
+     * @param ids 输入框
+     * @return true代表焦点在edit上
+     */
+    public boolean isFocusEditText(View v, int... ids) {
+        if (v instanceof EditText) {
+            EditText tmp_et = (EditText) v;
+            for (int id : ids) {
+                if (tmp_et.getId() == id) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 是否触摸在指定view上面,对某个控件过滤
+     *
+     * @param views
+     * @param ev
+     * @return
+     */
+    public boolean isTouchView(View[] views, MotionEvent ev) {
+        if (views == null || views.length == 0) {
+            return false;
+        }
+        int[] location = new int[2];
+        for (View view : views) {
+            view.getLocationOnScreen(location);
+            int x = location[0];
+            int y = location[1];
+            if (ev.getX() > x && ev.getX() < (x + view.getWidth())
+                    && ev.getY() > y && ev.getY() < (y + view.getHeight())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 是否触摸在指定view上面,对某个控件过滤
+     *
+     * @param ids
+     * @param ev
+     * @return
+     */
+    public boolean isTouchView(int[] ids, MotionEvent ev) {
+        int[] location = new int[2];
+        for (int id : ids) {
+            View view = findViewById(id);
+            if (view == null) {
+                continue;
+            }
+            view.getLocationOnScreen(location);
+            int x = location[0];
+            int y = location[1];
+            if (ev.getX() > x && ev.getX() < (x + view.getWidth())
+                    && ev.getY() > y && ev.getY() < (y + view.getHeight())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //endregion
+
+    //region 右滑返回上级
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            if (isTouchView(filterViewByIds(), ev)) {
+                return super.dispatchTouchEvent(ev);
+            }
+            if (hideSoftByEditViewIds() == null || hideSoftByEditViewIds().length == 0) {
+                return super.dispatchTouchEvent(ev);
+            }
+            View v = getCurrentFocus();
+            if (isFocusEditText(v, hideSoftByEditViewIds())) {
+                if (isTouchView(hideSoftByEditViewIds(), ev)) {
+                    return super.dispatchTouchEvent(ev);
+                }
+                //隐藏键盘
+                KeyboardUtils.hideSoftInput(this);
+                clearViewFocus(v, hideSoftByEditViewIds());
+
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+
+    }
+
+    /**
+     * 传入EditText的Id
+     * 没有传入的EditText不做处理
+     *
+     * @return id 数组
+     */
+    public int[] hideSoftByEditViewIds() {
+        return null;
+    }
+
+
+    /**
+     * 传入要过滤的View
+     * 过滤之后点击将不会有隐藏软键盘的操作
+     *
+     * @return id 数组
+     */
+    public View[] filterViewByIds() {
+        return null;
+    }
+
 
     /**
      * 为toolbar设置menu项
